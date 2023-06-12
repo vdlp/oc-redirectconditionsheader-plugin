172d928850a3516a77aa1e338420c48cd1fb1b24
Alwin Drenth <adrenth@gmail.com>
2023-06-12 12:54:57 +0200

    Add english and dutch language file

---
+++ lang/en/lang.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'header' => [
+        'description' => 'Header',
+        'explanation' => 'Match on HTTP header.',
+        'label' => 'Header name',
+        'value_label' => 'Header value',
+        'use_regex_label' => 'Use regular expression for header value.',
+        'use_regex_comment' => 'See <a href="http://php.net/manual/en/function.preg-match.php" target="_blank">preg_match</a> documentation.',
+    ],
+];
